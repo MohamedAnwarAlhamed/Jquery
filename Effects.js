8e0f6e3db4a726abc7e56ce5afc93bc280ad46fc
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-30 22:51:42 +0300

    Effects 1

---
+++ Effects.js
@@ -0,0 +1,12 @@
+// $ == jquery
+$(document).ready(function() {
+
+    $("nameofele").click(function() {
+        $("nameofele").hide();
+    })
+   
+
+});
+// ====================
+// https://www.w3schools.com/jquery/jquery_ref_effects.asp
+
