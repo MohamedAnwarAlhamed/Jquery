9ed567ae3f8254f782cdb147104cd07d958bd538
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-30 22:49:13 +0300

    Events 1

---
+++ Events.js
@@ -0,0 +1,12 @@
+// $ == jquery
+$(document).ready(function() {
+
+    $("nameofele").click(function() {
+        // code
+    })
+   
+
+});
+// ====================
+// https://www.w3schools.com/jquery/jquery_ref_events.asp
+
