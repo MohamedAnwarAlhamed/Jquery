f8423f9c11b4656db7a9448381160890dc1a44dc
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-30 22:34:01 +0300

    begin 1

---
+++ beginning.js
@@ -0,0 +1,17 @@
+// what is jQuery.?
+// jQuery features
+// download jQuery
+// Syntax
+// Selectors
+// Events
+// =====================================
+//Effects
+// [hide show fade slide animate stop ]
+// callback
+// chaining
+//======================================
+// Dom Manipulation
+// Get - Set
+// add [append prepend after before ] remove [remove empty]
+// css [addClass - removeClass - toggleClass - .css]
+// dimension [width - innerWidth - outerWidth(true)] 
