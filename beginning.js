1544ee2ecc8d99712abfadfbf058ed0846d27e5d
Mohamed_Anwar <alhamed202024@gmail.com>
2023-01-31 23:32:54 +0300

    begin 2

--- beginning.js
+++ beginning.js
@@ -15,3 +15,12 @@
 // add [append prepend after before ] remove [remove empty]
 // css [addClass - removeClass - toggleClass - .css]
 // dimension [width - innerWidth - outerWidth(true)] 
+
+//
+// Traversing
+// Ancestors [parent - parents - parentsUntil]
+// Descendants [ children - find]
+// Siblings [ siblings - next - nextAll -prev - prevAll - nextUntil - prevUntil]
+// Filter [first, last, eq, filter, not] 
+
+// jQuery Ajax
